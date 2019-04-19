7b0baefe9c6365c6913e79e1ed4c41ca8c8a7f6a
mj369gtl <michaeljamarcollege@gmail.com>
2019-04-18 18:44:24 -0700

    Testing changes

--- CECS 343 Team 4/src/CECS343Team4.java
+++ CECS 343 Team 4/src/CECS343Team4.java
@@ -8,14 +8,51 @@
  *
  * @author Judy
  */
+import java.util.Scanner;
 public class CECS343Team4 {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("testing");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        // 3 possible interfaces
+        /*
+        * one for superuser
+        * one for admin
+        * one for student
+        */
+        
+        int a = 0;
+        int id;
+        System.out.println("Login");
+        
+        sc.nextInt(a);
+        
+        switch(a){
+            case 1:
+                break;
+                
+            case 2:
+                break;
+                
+            case 3:
+                break;
+        }
+        
+        
+        public static void Menu(){
+            
+        }
+        
+        
+        
+        
+        
     }
     
 }
