6c2fe8aaac90df533136db96f7035ee03dd4f1f4
cemende <48813314+cemende@users.noreply.github.com>
2019-04-30 19:03:07 -0700

    Building contains()

--- CECS 343 Team 4/src/Building.java
+++ CECS 343 Team 4/src/Building.java
@@ -3,22 +3,30 @@ import java.util.ArrayList;
 public class Building {
     private String buildingName;
     private int occupancyLimit;
-    private ArrayList <String> rooms = new ArrayList();
+    private int totalOccupancy;
+    private ArrayList <Room> rooms = new ArrayList();
     
     public Building(String name, int limit) {
         occupancyLimit = limit;
         buildingName = name;
+        totalOccupancy = 0;
     }
     
-    public void addRoom(String room) {
+    public boolean isEmpty() {
+        return rooms.isEmpty();
+    }
+    
+    public void addRoom(Room room) {
         rooms.add(room);
+        totalOccupancy += room.getOccupancyLimit();
     }
     
-    public void removeRoom(String room) {
+    public void removeRoom(Room room) {
         rooms.remove(room);
+        totalOccupancy -= room.getOccupancyLimit();
     }
     
-    public ArrayList<String> getRoomList() {
+    public ArrayList<Room> getRoomList() {
         return rooms;
     }
     
@@ -27,11 +35,21 @@ public class Building {
             return "No Rooms";
         } else {
             String s = "";
-            for(String room: rooms){
-                s += room + "\n";
+            for(Room room: rooms){
+                s += room.getRoomName() + "\n";
             }
             return s;
         }
+    }
+    
+    public Room contains(String s) {
+        Room temp = null;
+        for(Room room : rooms) {
+            if(room.getRoomName().equals(s)) {
+                temp = room;
+            }
+        }
+        return temp;
     }
     
     public int getOccupancyLimit() {
